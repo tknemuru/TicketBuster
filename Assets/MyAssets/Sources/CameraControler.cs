e88042b65d29821500b27f195a310ed06188e5f7
tknemuru <kojikoji.max.5252@gmail.com>
2016-02-19 20:39:28 +0900

    camera change

---
+++ Assets/MyAssets/Sources/CameraControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+    private Vector3 Difference { get; set; }
+
+    // Use this for initialization
+	void Start () {
+        this.Difference = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("SciFi_Fighter_AK5"))
+        {
+            Vector3 startVec = GameObject.Find("SciFi_Fighter_AK5").transform.localPosition;
+            transform.localPosition = new Vector3(this.Difference.x, startVec.y + this.Difference.y, startVec.z + this.Difference.z);
+        }
+    }
+}
