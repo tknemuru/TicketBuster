601651f3e40be360057c4593a46b70e576640f70
tknemuru <kojikoji.max.5252@gmail.com>
2016-02-19 23:08:15 +0900

    camera adjust

---
+++ Assets/MyAssets/Sources/Main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+    public GameObject StarFighter;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(StarFighter);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
