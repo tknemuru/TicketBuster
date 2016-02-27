40beee8cad9f7d96a9918cfb029018988da85285
tknemuru <kojikoji.max.5252@gmail.com>
2016-02-27 22:05:47 +0900

    scene change

--- Assets/MyAssets/Sources/Preview.cs
+++ Assets/MyAssets/Sources/Preview.cs
@@ -7,13 +7,27 @@ public class Preview : MonoBehaviour {
     /// </summary>
     public GameObject StarFighter;
 
+    /// <summary>
+    /// 敵
+    /// </summary>
+    public GameObject Enemy;
+
+    private float EnemyIntervalTime { get; set; }
+
 	// Use this for initialization
 	public void Start () {
+        this.EnemyIntervalTime = 0.0f;
         var fighter = (GameObject)Instantiate(StarFighter, new Vector3(0.0f, 2.86f, -6.0f), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	public void Update () {
-	
+        Quaternion quat = Quaternion.Euler(0, 180, 0);
+        EnemyIntervalTime += Time.deltaTime;
+        if (EnemyIntervalTime >= 4.0f)
+        {
+            EnemyIntervalTime = 0;
+            var enemy = (GameObject)Instantiate(Enemy, new Vector3(transform.position.x, transform.position.y, transform.position.z + 200), quat);
+        }
 	}
 }
---
+++ Assets/MyAssets/Sources/UserNameButtonController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UserNameButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    public void Update()
+    {
+	
+	}
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Preview");
+    }
+}
